36f04bf708a205af742d81e3ac26d4de5d7adfc2
ansarmirza077 <108061631+ansarmirza077@users.noreply.github.com>
2022-08-28 19:44:16 +0530

    Create Freq_else.c

---
+++ Freq_else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+  {
+  int a[10],i,freq=0,key;
+  for(i=0;i<10;i++)
+    {
+    printf("Enter number");
+    scanf("%d",a[i]);
+    }
+    printf("Enter no to find freq");
+    scanf(%d",&key);
+          for(i=0;i<10;i++)
+          ig(a[i]==key)
+          freq++;
+          printf("Freq of %d is %d ",key,freq);
+          return 0;
+          }
