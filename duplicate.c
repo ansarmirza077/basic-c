ee019dce5add62f55e1adea88d6f87125c8aae45
ansarmirza077 <108061631+ansarmirza077@users.noreply.github.com>
2022-08-29 00:18:01 +0530

    Create duplicate.c

---
+++ duplicate.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int arr[20];
+int n,i,count,d,x;
+printf(Enter no of elements ");
+scanf("%d", & arr[i]);
+printf(" Duplicates are \n");
+for(i=0;i<n;i++)
+{
+count =0;
+d=arr[i];
+x=i;
+for(i=0;i<n;i++)
+{
+if(d==arr[i];
+++count;
+}
+i=x;
+if(count>1)
+printf("%d",times \n,d,count);
+}
+return 0;
+}
